b2e6150d26f69b76c4b32a8f7eb18db22323861f
Vikas_Singh <Vikas_Singh@VikasPC>
2022-12-16 11:07:49 -0800

    Updated

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,19 +6,15 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-    private Boolean topping;
-    private Boolean cheese;
-    private Boolean paper_bag;
+    private boolean isToppingAdded;
+    private boolean isCheeseAdded;
+    private boolean isPaperBagAdded;
     private int topping_charge;
-    private Boolean isBillGenerated;
+    private boolean isBillGenerated;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         this.price = isVeg? 300: 400;
-        cheese = false;
-        topping = false;
-        paper_bag = false;
-        isBillGenerated = false;
         bill = ("Base Price Of The Pizza: "+this.price+"\n");
     }
 
@@ -27,41 +23,44 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        if(cheese) return;
-        cheese = true;
-        price += 80;
+        if(!isCheeseAdded){
+            isCheeseAdded = true;
+            price += 80;
+        }
     }
 
     public void addExtraToppings(){
-        if(topping) return;
-        topping = true;
+        if(!isToppingAdded) {
+        isToppingAdded = true;
         topping_charge = isVeg ? 70 : 120;
         price += topping_charge;
+        }
     }
 
     public void addTakeaway(){
-        if(paper_bag) return;
-         paper_bag = true;
+        if(!isPaperBagAdded) {
+         isPaperBagAdded = true;
          price += 20;
+        }
     }
 
     public String getBill(){
 
         if(isBillGenerated) return bill;
-        isBillGenerated = true;
-         if(cheese) {
+         if(isCheeseAdded) {
              bill += ("Extra Cheese Added: "+80+"\n");
          }
          
-         if(topping) {
+         if(isToppingAdded) {
              bill += ("Extra Toppings Added: "+topping_charge+"\n");
          }
          
-         if(paper_bag) {
+         if(isPaperBagAdded) {
              bill += ("Paperbag Added: "+20+"\n");
          }
          
          bill += ("Total price: "+price+"\n");
+         isBillGenerated = true;
          return bill;
     }
 }
